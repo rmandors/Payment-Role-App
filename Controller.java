a1aa3742b9c358f48ffafe7755e74d22d5f7f4bd
nikotov <nicolastovar18@hotmail.com>
2025-04-18 23:05:56 -0500

    GUI and Controller

---
+++ Controller.java
@@ -0,0 +1,66 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField comissionField;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Button deleteReg;
+
+    @FXML
+    private Button export;
+
+    @FXML
+    private Label fullNameField;
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private TextField lastnameField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Button newReg;
+
+    @FXML
+    private ListView<?> regListView;
+
+    @FXML
+    private TextField salaryField;
+
+    @FXML
+    private TextField titleField;
+
+    @FXML
+    private ComboBox<?> typeComboBox;
+
+    @FXML
+    void deleteReg(ActionEvent event) {
+
+    }
+
+    @FXML
+    void export(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newReg(ActionEvent event) {
+
+    }
+
+}
