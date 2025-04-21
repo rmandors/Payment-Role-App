3f5dbccb158325ca338411d1c9c4da1464107933
Raymond Portilla <134645362+rmandors@users.noreply.github.com>
2025-04-20 19:46:02 -0500

    Update Manager.java

--- model/Manager.java
+++ model/Manager.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;  
 import javax.xml.bind.annotation.XmlRootElement;  
 
-@XmlRootElement
+@XmlRootElement(name="manager")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Manager extends Employee {
     private String educationLevel;
