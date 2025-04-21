200d47cb95af0b61384fdb4d5fb752b8616ef20a
nikotov <nicolastovar18@hotmail.com>
2025-04-21 08:49:51 -0500

    Minor changes

--- controller/GUIObservers.java
+++ controller/GUIObservers.java
@@ -1,8 +1,5 @@
 package controller;
 
-import javafx.beans.Observable;
-import javafx.beans.value.ObservableValue;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import model.Employee;
 
@@ -27,17 +24,5 @@ public class GUIObservers {
         });
         
     }
-
-/*     static void formatObserver(Controller controller, ComboBox<String> comboBox){
-        comboBox.getSelectionModel().selectedItemProperty().addListener(
-            (ObservableValue<? extends String> _, String _, String newValue) -> {
-                if (newValue == "Consola (Unitario)") {
-                    controller.exportData.setText("Buscar");
-                }
-                else {
-                    controller.exportData.setText("Exportar");
-                }
-        });
-    } */
 
 }
